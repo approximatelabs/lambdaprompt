35e23e71d8858c827bf340d05e9376c05ff8a4f2
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2023-03-01 13:23:12 -0700

    add chatgpt endpoint support

--- lambdaprompt/__init__.py
+++ lambdaprompt/__init__.py
@@ -1,19 +1,16 @@
 import nest_asyncio
 
-from .gpt3 import AsyncGPT3Prompt, GPT3Prompt
-from .prompt import (
-    AsyncPrompt,
-    Prompt,
-    prompt,
-    register_call_callback,
-    register_creation_callback,
-)
+from .gpt3 import AsyncGPT3Chat, AsyncGPT3Prompt, GPT3Chat, GPT3Prompt
+from .prompt import (AsyncPrompt, Prompt, prompt, register_call_callback,
+                     register_creation_callback)
 
 nest_asyncio.apply()
 
 __all__ = [
     "AsyncGPT3Prompt",
     "GPT3Prompt",
+    "AsyncGPT3Chat",
+    "GPT3Chat",
     "AsyncPrompt",
     "Prompt",
     "prompt",
--- lambdaprompt/gpt3.py
+++ lambdaprompt/gpt3.py
@@ -1,11 +1,13 @@
 import asyncio
+import json
 import os
 
 import aiohttp
+import yaml
 from jinja2 import Environment, meta
 
 from .config import CONFIG
-from .prompt import AsyncPrompt, PromptTemplate, get_uid_from_obj
+from .prompt import AsyncPrompt, Prompt, PromptTemplate, get_uid_from_obj
 
 env = Environment()
 
@@ -61,6 +63,62 @@ async def async_get_gpt3_response(
                     else:
                         print(f"Not sure what happened: {answer}")
     return get_gpt3_response_choice(answer)
+
+
+# message = {'role': ('system', 'user', 'assistant'), 'content': 'text'}
+def get_gpt3_chat_reqs(
+    messages, temperature=0.0, stop=None, model_name="gpt-3.5-turbo"
+):
+    if not os.environ.get("OPENAI_API_KEY"):
+        raise Exception("No OpenAI API key found")
+    headers = {
+        "Authorization": f"Bearer {os.environ.get('OPENAI_API_KEY')}",
+        "Content-Type": "application/json",
+    }
+    data = {
+        "messages": messages,
+        "max_tokens": 500,
+        "temperature": temperature,
+        "model": model_name,
+        "presence_penalty": 0.2,
+        "frequency_penalty": 0.2,
+    }
+    if stop:
+        data["stop"] = stop
+    return headers, data
+
+
+def get_gpt3_chat_response_choice(answer):
+    # assuming that we get an assistant response? 
+    # TODO: validate Is it always that?
+    if "choices" in answer:
+        return answer["choices"][0]["message"]['content']
+    else:
+        print("Possible error: query returned:", answer)
+    return ""
+
+
+async def async_get_gpt3_chat_response(
+    messages, temperature=0.0, stop=None, model_name="gpt-3.5-turbo"
+):
+    headers, data = get_gpt3_chat_reqs(messages, temperature, stop, model_name)
+    trying = 0
+    while trying < 4:
+        trying += 1
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            async with session.post(
+                "https://api.openai.com/v1/chat/completions", headers=headers, json=data
+            ) as resp:
+                answer = await resp.json()
+                if "choices" in answer:
+                    return get_gpt3_chat_response_choice(answer)
+                else:
+                    if "Rate limit" in answer.get("error", {}).get("message", ""):
+                        print(".", end="")
+                        await asyncio.sleep(trying * 10)
+                    else:
+                        print(f"Not sure what happened: {answer}")
+    return get_gpt3_chat_response_choice(answer)
 
 
 def get_gpt3_edit_reqs(
@@ -128,6 +186,73 @@ class GPT3Prompt(PromptTemplate):
 
 
 class AsyncGPT3Prompt(GPT3Prompt, AsyncPrompt):
+    pass
+
+
+class GPT3Chat(PromptTemplate):
+    def __init__(
+        self,
+        base_conversation=None,
+        name=None,
+        temperature=0.0,
+        stop=None,
+        model_name="gpt-3.5-turbo",
+    ):
+        if isinstance(base_conversation, str):
+            base_conversation = yaml.safe_load(base_conversation)
+
+        base_conversation = [] if base_conversation is None else base_conversation
+        assert isinstance(base_conversation, list)
+        assert all([isinstance(x, dict) for x in base_conversation])
+        # right now, assume its set up as {'system': 'hello {{user}}}', 'user': 'Hey there!', 'assistant': 'Hi!'}
+        assert all([k in ['user', 'assistant', 'system'] for x in base_conversation for k in x.keys()])
+        name = name or f"GPT3Chat_{get_uid_from_obj(base_conversation)}"
+
+        self.kwargs = {'temperature': temperature, 'stop': stop, 'model_name': model_name}
+        self.roles = [next(iter(template.keys())) for template in base_conversation]
+        self.message_template_strings = [next(iter(template.values())) for template in base_conversation]
+        self.message_templates = [env.from_string(x) for x in self.message_template_strings]
+        
+        async def function(user_input=None, **prompt_kwargs):
+            messages = self.resolve_templated_conversation(user_input, **prompt_kwargs)
+            return await async_get_gpt3_chat_response(
+                messages, temperature=temperature, model_name=model_name, stop=stop
+            )
+
+        super(PromptTemplate, self).__init__(function, name=name)
+
+    def resolve_templated_conversation(self, user_input=None, **prompt_kwargs):
+        messages = []
+        for i, template in enumerate(self.message_templates):
+            messages.append({'role': self.roles[i], 'content': template.render(**prompt_kwargs)})
+        if user_input is not None:
+            messages.append({'role': 'user', 'content': user_input})
+        return messages
+    
+    def add(self, user=None, assistant=None, system=None):
+        # check that only 1 user, assistant or system is defined
+        assert sum([user is not None, assistant is not None, system is not None]) == 1, "only define one"
+        new_base_conversation = [{k: v} for k, v in zip(self.roles, self.message_template_strings)]
+        if user is not None:
+            new_base_conversation.append({'user': user})
+        if assistant is not None:
+            new_base_conversation.append({'assistant': assistant})
+        if system is not None:
+            new_base_conversation.append({'system': system})
+        return self.__class__(new_base_conversation, **self.kwargs)
+
+    def get_named_args(self):
+        variables = []
+        for template_string in self.message_template_strings:
+            variables.extend(meta.find_undeclared_variables(env.parse(template_string)))
+        return variables
+    
+    @property
+    def code(self):
+        return json.dumps([{k: v} for k, v in zip(self.roles, self.message_template_strings)])
+
+
+class AsyncGPT3Chat(GPT3Chat, AsyncPrompt):
     pass
 
 
