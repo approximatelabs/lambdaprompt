a80f99d0087f71f9eabc0f85b7414e5d8152fcd7
Justin Waugh <916073+bluecoconut@users.noreply.github.com>
2023-05-12 09:42:51 +0000

    no longer using config concept. up to you to

--- tests/test_gpt3.py
+++ tests/test_gpt3.py
@@ -6,11 +6,6 @@ from aioresponses import aioresponses
 from lambdaprompt import AsyncGPT3Prompt, GPT3Prompt, prompt, register_call_callback
 
 
-@pytest.fixture(autouse=True)
-def ignore_load_dotenv(mocker):
-    mocker.patch("lambdaprompt.config.load_dotenv", return_value=None)
-
-
 def test_api_key_needed():
     with aioresponses() as m:
         m.post(
